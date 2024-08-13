ea61df0c24979f92d0c9385346ac5be4969de631
Shakib <shakib2501@gmail.com>
2024-08-13 11:00:01 +0530

    Updated the parser and message format conditions

--- src/Parser.py
+++ src/Parser.py
@@ -7,7 +7,7 @@ import pandas as pd
 class Parser:
     def __init__(self, file):
         self.all_trades = dict()
-        self.filename = gzip.open(os.path.join('..', 'datafile', file), 'rb')
+        self.filename = gzip.open(file, 'rb')
 
     def parseTime(self, timestamp):
         return struct.unpack('>Q', struct.pack('2s6s', b'\x00\x00', timestamp))[0];
@@ -33,31 +33,45 @@ class Parser:
 
 
     def messageEventMap(self):
-        msg_type = {'S':11,'R':38,'H':24, 'A':39, 'F':30, 'E':35, 'C':22, 'D':18, 'U':34, 'P':43, 'Q':39}
-    def parseTrade(self, file):
+        msg_type = {'S':11,'R':38,'H':24,'Y':19,'L':25,'V':34, 'W':11, 'K':27,'J':34,'h':20, 'A':35, 'F':39, 'E':30, 'C':35,'X':22, 'D':18, 'U':34, 'P':43, 'Q':39,'B':18,'I':49,'N':19}
+        return msg_type
 
+
+    def parseTrade(self):
+        open_time = 0
+        close_time = 0
         stocks = dict()
         total_orders = []
-
         orders = dict()
-
-        msg_type = file.read(1)
+        msg_type = self.filename.read(1)
+        # print('msg_type', msg_type)
+        # print(type(msg_type))
         market_opened = False
         me_map = self.messageEventMap()
+        # me_map = self.messageMap()
+
+        mb = 1024*1024
+        freq = 100*1024*1024
+        total_bytes = 0
+        running_bytes = 0
+        cnt = 0
         while msg_type:
             msg_type = msg_type.decode()
-            if msg_type not in me_map:
-                continue
-            else:
+            # print(cnt)
+            if(running_bytes > freq):
+                print("%d MB data parsed " % (total_bytes/mb))
+                running_bytes = running_bytes/freq
+            if msg_type in me_map.keys():
                 tot_offset = me_map[msg_type]
-                msg = file.read(tot_offset)
-                if msg_type == 'S':
+                msg = self.filename.read(tot_offset)
+                total_bytes += tot_offset
+                running_bytes += tot_offset
+                if msg_type == "S":
                     data = struct.unpack('>HH6sc', msg)
-                    #Check if market has started
                     if(data[3].decode() == 'Q'):
                         open_time = self.parseTime(data[2])
                         market_opened = True
-                    else:
+                    elif data[3].decode() == "M":
                         close_time = self.parseTime(data[2])
                         break
                 elif msg_type == 'R':
@@ -66,20 +80,20 @@ class Parser:
                     stocks[stock_id] = data[3].decode().strip()
                 elif msg_type == 'A':
                     data = struct.unpack('>HH6sQcI8sI', msg)
-                    reference = data[2]
-                    price = data[7]/(10**4)
-                    orders[reference] = price
+                    reference = data[3]
+                    orders[reference] = data[7]/(10**4)
+                    # orders[reference] = price
                 elif msg_type == 'F':
                     data = struct.unpack('>HH6sQcI8sI4s', msg)
-                    reference = data[2]
+                    reference = data[3]
                     price = data[7]/(10**4)
                     orders[reference] = price
                 elif msg_type == 'U':
                     data = struct.unpack('>HH6sQQII', msg)
-                    reference = data[2]
-                    price = data[7]/(10**3)
+                    reference = data[4]
+                    price = data[6]/(10**4)
                     orders[reference] = price
-                elif msg_type == 'E':
+                elif msg_type == 'E' and market_opened:
                     data = struct.unpack('>HH6sQIQ', msg)
                     stock_id = data[0]
                     time = self.parseTime(data[2])
@@ -89,7 +103,7 @@ class Parser:
                     total_orders.append([stock_id, time, price, no_of_shares])
                 elif msg_type == 'C' and market_opened:
                     data = struct.unpack('>HH6sQIQcI', msg)
-                    is_printable = data[0].decode().strip()
+                    is_printable = data[6].decode().strip()
                     if(is_printable == 'Y'):
                         stock_id = data[0]
                         time = self.parseTime(data[2])
@@ -103,8 +117,12 @@ class Parser:
                     no_of_shares = data[4]
                     price = data[7]/(10**4)
                     total_orders.append([stock_id, time, price, no_of_shares])
-            msg_type = file.read(1)
+            msg_type = self.filename.read(1)
+            total_bytes += 1
+            running_bytes += 1
+            # cnt += 1
         return stocks, total_orders, close_time
+
 
     def VWAP(self, trade, c_val=0, c_qty=0):
         trade_price, trade_quantity = trade
@@ -117,10 +135,9 @@ class Parser:
         return c_val, c_qty, average_price
 
     def calculate_vwap(self):
-        stock_map, total_orders, close_time = self.parseTrade(self.filename)
-        self.initialize(stock_map.key())
+        stock_map, total_orders, close_time = self.parseTrade()
+        self.initialize(stock_map.keys())
         self.parse_total_orders(total_orders, close_time)
-        output_filename = self.filename.split(',')[0]
         output = pd.DataFrame()
         VWAP_value_map = dict()
         stocks = []
@@ -136,13 +153,15 @@ class Parser:
                 VWAP_value_map[n].append(vwap)
         output['Stock'] = stocks
         for n in range(10, 17):
-            vwaps = vwap[n]
+            vwaps = VWAP_value_map[n]
             output["%s Running VWAP" % n] = vwaps
 
-        output.to_csv(os.path.join('..','datafile',output_filename, index=False))
+        output.to_csv(os.path.join('..', 'output', 'output_vwap.csv'), index=False)
 
 
 
 if __name__ == '__main__':
-    parser = Parser('01302019.NASDAQ_ITCH50.gz')
-
+    # data = gzip.open(os.path.join('..', 'datafile', '01302019.NASDAQ_ITCH50.gz'), 'rb')
+    filename = os.path.join('..', 'datafile', '01302019.NASDAQ_ITCH50.gz')
+    parser = Parser(filename)
+    parser.calculate_vwap()
